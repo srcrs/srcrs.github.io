1ed681b7e0cc6d584d6c4c78cdaf4cb3435e774b
srcrs <srcrs@foxmail.com>
2020-01-27 16:18:16 +0800

    update title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,15 +3,17 @@ module.exports = {
   "description": "A place with all of me",
   "dest": "public",
   "base": "/",
-  "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
-    ],
-    [
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+	[
       "meta",
       {
         "name": "viewport",
@@ -19,6 +21,7 @@ module.exports = {
       }
     ]
   ],
+
   "theme": "reco",
   "themeConfig": {
     "nav": [
@@ -78,6 +81,9 @@ module.exports = {
     "@vuepress/medium-zoom",
     "flowchart",
     "ribbon",
+	['@vuepress/pwa', {
+      serviceWorker: true
+    }],
 	["@vuepress-reco/loading-page", false] //去掉动画
   ]
 }
