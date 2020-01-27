79a5045941545e151ab962065ee68c5fb50411fa
srcrs <srcrs@foxmail.com>
2020-01-27 16:52:18 +0800

    update title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,16 +3,14 @@ module.exports = {
   "description": "A place with all of me",
   "dest": "public",
   "base": "/",
-  head: [
-    ['link', { rel: 'icon', href: '/logo.png' }],
+    head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['meta', { name: 'application-name', content: 'srcrs' }],
+	['meta', { name: 'apple-mobile-web-app-title', content: 'srcrs' }],
+    ['meta', { name: 'msapplication-starturl', content: '/' }],
 	[
       "meta",
       {
