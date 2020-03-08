117c7e87077cde719546768abb2a1e3e592dd20e
srcrs <srcrs@foxmail.com>
2020-03-08 13:46:05 +0800

    update title

--- srcrs.sh
+++ srcrs.sh
@@ -1,5 +1,5 @@
 str="update title"
-uri="https://d2b4c67b9f34c2d69ee0281efe296b74f6ec861@github.com/srcrs/srcrs.github.io.git vue"
+uri="https://github.com/srcrs/srcrs.github.io.git vue"
 set -e
 git add .
 git commit -m "$str"
