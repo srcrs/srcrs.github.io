5ea2164219185d26d283a28423fa46eea9e0c014
srcrs <srcrs@foxmail.com>
2020-06-04 00:59:04 +0800

    update title

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -7,9 +7,9 @@ module.exports = {
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['link', { rel: 'apple-touch-icon', href: '/img/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/img/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/img/msapplication-icon-144x144.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
 	['meta', { name: 'viewport', 'content': 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
@@ -35,7 +35,7 @@ module.exports = {
       // organization: 'Xi\'an Jiao Tong University',
 
       // 头像
-      avatar: 'favicon.svg',
+      avatar: 'head.svg',
 
       // 社交平台帐号信息
       sns: {
