192250033485d274e7ed235fc7fb233dba66ed9a
srcrs <srcrs@foxmail.com>
2020-07-16 08:53:08 +0800

    update_title

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -74,7 +74,7 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/', exact: true },
       { text: 'Posts', link: '/posts/', exact: false },
-      { text: 'Custom Pages', link: '/custom-pages/', exact: false },
+      { text: 'Share', link: '/Share/', exact: false },
       { text: 'Github', link: 'https://github.com/srcrs/srcrs.github.io' },
     ],
 
