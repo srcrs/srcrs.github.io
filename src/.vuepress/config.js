d84a3d0beff1c3bc2c5ed283dfa1b3ea6b3c4a7e
edz <edz@192.168.14.117>
2021-04-11 03:35:40 +0800

    使用cdn加速静态资源访问

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,15 +1,16 @@
+const path=require('path')
 module.exports = {
   title: "srcrs",
   description: "A whole place about me",
    head: [
-    ['link', { rel: 'icon', href: '/assets/img/favicon.svg' }],//站点图标
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['link', { rel: 'icon', href: 'https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master/assets/img/favicon.svg' }],//站点图标
+    ['link', { rel: 'manifest', href: 'https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/assets/img/apple-touch-icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/assets/img/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/assets/img/msapplication-icon-144x144.png' }],
+    ['link', { rel: 'apple-touch-icon', href: 'https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master/assets/img/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: 'https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master/assets/img/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: 'https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master/assets/img/msapplication-icon-144x144.png' }],
     ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
 	['meta', { name: 'viewport', 'content': 'width=device-width,initial-scale=1,user-scalable=no' }]
   ],
@@ -35,7 +36,7 @@ module.exports = {
       // organization: 'Xi\'an Jiao Tong University',
 
       // 头像
-      avatar: '/assets/img/head.svg',
+      avatar: 'https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master/assets/img/head.svg',
 
       // 社交平台帐号信息
       sns: {
@@ -52,7 +53,7 @@ module.exports = {
       // header 的背景，可以使用图片，或者随机变化的图案（geopattern）
       background: {
         // 使用图片的 URL，如果设置了图片 URL，则不会生成随机变化的图案，下面的 useGeo 将失效
-        url: '/assets/img/background.svg',
+        url: 'https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master/assets/img/background.svg',
         // 使用随机变化的图案，如果设置为 false，且没有设置图片 URL，将显示为空白背景
          useGeo: false,
       },
@@ -99,6 +100,20 @@ module.exports = {
       // 是否允许主题自动添加 Posts 页面 (url: /posts/)
       posts: true,
     },
+  },
+  //加速静态资源
+  configureWebpack: () => {
+    return {
+      output: {
+        publicPath: 'https://cdn.jsdelivr.net/gh/srcrs/srcrs.github.io@master'
+      },
+      resolve: {
+        alias: {
+          'dist': path.resolve(__dirname, './dist')
+
+        }
+      }
+    }
   },
   "plugins": [
     "vuepress-theme-meteorlxy",
