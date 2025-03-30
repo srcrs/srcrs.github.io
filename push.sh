5cd689f79e1536f94713dd364c6f50baa77d6d2c
srcrs <srcrs@foxmail.com>
2025-03-30 07:52:23 -0400

    update

--- push.sh
+++ push.sh
@@ -1,1 +1,11 @@
-docker cp doduocc-server:/usr/share/nginx/html/ .
+docker cp hugo-server:/usr/share/nginx/html/ .
+
+cp -r html/* .
+
+rm -rf html
+
+git add .
+
+git commit -m "update"
+
+git push
